46cdbf704c8a23a45fe3375016f42b6bdda4680e
4015e9 <4015e9@icaco.ece.uprm.edu>
2017-11-15 11:23:09 -0400

    created an arrow and fill it with colors

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel {
@@ -34,8 +36,19 @@ public class MyPanelClass extends JPanel {
                         g.setColor(Color.WHITE);
                         g.drawLine(x1, y1, x2, y2);
                         
+//                        g.setColor(Color.YELLOW);
+//                        g.fillRoundRect((width/2) -27, (height/2) -27, 55, 55, getWidth(), getHeight());
+                      
+                        Polygon p = new Polygon();
+                        p.addPoint(x1 + 5, y1 + 25);
+                        p.addPoint(x1 + 20, y1 + 10);
+                        p.addPoint(x1 + 35, y1 + 25);
+                        p.addPoint(x1 + 25, y1 + 25);
+                        p.addPoint(x1 + 25, y1 + 45);
+                        p.addPoint(x1 + 15, y1 + 45);
+                        p.addPoint(x1 + 15, y1 + 25);
                         g.setColor(Color.YELLOW);
-                        g.fillRoundRect((width/2) -27, (height/2) -27, 55, 55, getWidth(), getHeight());
-
+                        g.fillPolygon(p);
+                        
                                     }
 }

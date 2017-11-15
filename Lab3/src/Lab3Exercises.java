242fe76553c0c35bd55edd690957f0e74c91a5f7
4015e9 <4015e9@icaco.ece.uprm.edu>
2017-11-15 10:29:59 -0400

    eliminated the comment slashes in setDeafultCloseoperation, changed the location and size of myFrame

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,9 +2,9 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
+		JFrame myFrame = new JFrame("Roberto CArlos Hernandez Martinez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(840, 540);
 		myFrame.setSize(400, 100);
 		myFrame.setVisible(true);
 	}
